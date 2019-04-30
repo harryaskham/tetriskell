77b6282c09b931cc26d53fe20d05151ad05e6cc7
Harry Askham <askham@google.com>
2019-04-30 13:03:44 +0100

    Tiny parts of initial library

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,34 @@
 module Lib
-    ( someFunc
+    ( emptyGrid
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List
+
+data Square = O | X deriving (Show)
+type Row = [Square]
+type Grid = [Row]
+type Coordinate = (Int, Int)
+type Piece = [Coordinate]
+
+-- |Creates an empty playing grid of the given dimensions.
+emptyGrid :: Int -> Int -> Grid
+emptyGrid x y = let row = take x $ repeat O in take y $ repeat row
+
+-- |The default playing field with a 4-line buffer.
+defaultGrid :: Grid
+defaultGrid = emptyGrid 10 24
+
+-- |A line piece in the bottom-left.
+linePiece :: Piece
+linePiece = [(0, 0), (0, 1), (0, 2), (0, 3)]
+
+-- |Places a piece on the grid if possible.
+withPiece :: Piece -> Grid -> Maybe Grid
+withPiece [] g = Just g
+withPiece (p:ps) g = do
+  g <- withCoordinate p g
+  withPiece ps g
+
+-- |Fills a single coordinate on the grid if possible.
+withCoordinate :: Coordinate -> Grid -> Maybe Grid
+withCoordinate (x, y) g = undefined
